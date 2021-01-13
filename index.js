a9250c56b988e102d08cfef983b0e19ca4aecdf4
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-01-12 16:54:35 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "Anne"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    console.log(customerName); 
+    return customerName
+} 
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+} 
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "John"
+}
